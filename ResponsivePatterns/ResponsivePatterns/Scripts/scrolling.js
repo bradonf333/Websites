d439fdba2605959dbee5f4faf45a6ce1a832bd49
bradonf333 <bradonf123@gmail.com>
2017-04-11 21:18:22 -0600

    Added Bootstrap scrollspy to the scrolling js

--- ResponsivePatterns/ResponsivePatterns/Scripts/scrolling.js
+++ ResponsivePatterns/ResponsivePatterns/Scripts/scrolling.js
@@ -2,4 +2,11 @@
     $(window).on("load resize", function () {
         $(".fill-screen").css("height", window.innerHeight);
     });
+
+    // Bootstrap ScrollSpy
+    $('body').scrollspy({
+        target: '.navbar'
+    })
+
+
 });
