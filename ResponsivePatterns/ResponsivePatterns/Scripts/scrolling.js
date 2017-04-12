6c6fd9243498bd00cb917def6e21b5a8067e2af9
bradonf333 <bradonf123@gmail.com>
2017-04-11 22:13:22 -0600

    Added jquery for animation scrolling

--- ResponsivePatterns/ResponsivePatterns/Scripts/scrolling.js
+++ ResponsivePatterns/ResponsivePatterns/Scripts/scrolling.js
@@ -13,4 +13,11 @@ $(function () {
 
     $('#content').scrollspy();
 
+    $('nav a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
 });
