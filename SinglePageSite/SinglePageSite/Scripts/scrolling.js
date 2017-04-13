eba1f832a37745b74e21b46d9c70ff31f89bc639
bradonf333 <bradonf123@gmail.com>
2017-04-12 20:39:47 -0600

    Fixed scrollspy

--- SinglePageSite/SinglePageSite/Scripts/scrolling.js
+++ SinglePageSite/SinglePageSite/Scripts/scrolling.js
@@ -4,19 +4,21 @@
     });
 
     // add Bootstrap's scrollspy
-    /*
+
     $('body').scrollspy({
-        target: '.navbar'
+        target: '.navbar',
+        offset: 900
+    });
+
+    /*
+    $('#content').scrollspy({
+        offset: 450
     });
     */
 
-    $('#content').scrollspy({
-        offset: 160
-    });
-
     $('nav a, .down-button a').bind('click', function () {
         $('html, body').stop().animate({
-            scrollTop: $($(this).attr('href')).offset().top - 100
+            scrollTop: $($(this).attr('href')).offset().top - 140
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
