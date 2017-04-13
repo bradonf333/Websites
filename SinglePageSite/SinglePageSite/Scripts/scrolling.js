d2227a1778ffa6deff9e6fb4ff0e094733821b8f
bradonf333 <bradonf123@gmail.com>
2017-04-12 20:10:27 -0600

    Added scrollspy js

--- SinglePageSite/SinglePageSite/Scripts/scrolling.js
+++ SinglePageSite/SinglePageSite/Scripts/scrolling.js
@@ -2,4 +2,23 @@
     $(window).on("load resize", function () {
         $(".fill-screen").css("height", window.innerHeight);
     });
+
+    // add Bootstrap's scrollspy
+    /*
+    $('body').scrollspy({
+        target: '.navbar'
+    });
+    */
+
+    $('#content').scrollspy({
+        offset: 160
+    });
+
+    $('nav a, .down-button a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
 });
